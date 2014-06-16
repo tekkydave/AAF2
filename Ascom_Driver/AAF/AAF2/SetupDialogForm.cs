16cc378a48197920081ef7f861cd75f3ea7fc278
tekkydave <tekkydave@96f2b3c2-ac1f-4b8f-8044-93e5e60fcbfa>
2014-06-16 13:39:04 +0000

    Initial Import

---
+++ Ascom_Driver/AAF/AAF2/SetupDialogForm.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using ASCOM.Utilities;
+using ASCOM.AAF2;
+
+namespace ASCOM.AAF2
+{
+    [ComVisible(false)]					// Form not registered for COM!
+    public partial class SetupDialogForm : Form
+    {
+        public SetupDialogForm()
+        {
+            InitializeComponent();
+            // Initialise current values of user settings from the ASCOM Profile 
+            comboBoxComPort.Items.Clear();
+            using (ASCOM.Utilities.Serial serial = new Utilities.Serial())
+            {
+                int i = 0;
+                foreach (var item in serial.AvailableCOMPorts)
+                {
+                    comboBoxComPort.Items.Add(item);
+                    if (item == Focuser.comPort)
+                    {
+                        comboBoxComPort.SelectedIndex = i;
+                    }
+                    i++;
+                }
+            }
+
+            chkTrace.Checked = Focuser.traceState;
+        }
+
+        private void cmdOK_Click(object sender, EventArgs e) // OK button event handler
+        {
+            // Place any validation constraint checks here
+
+            Focuser.comPort = comboBoxComPort.Text; // Update the state variables with results from the dialogue
+            Focuser.traceState = chkTrace.Checked;
+
+            using (ASCOM.Utilities.Profile p = new Utilities.Profile())
+            {
+                p.DeviceType = "Focuser";
+                p.WriteValue(Focuser.driverID, "ComPort", (string)comboBoxComPort.Text);
+            }
+        }
+
+        private void cmdCancel_Click(object sender, EventArgs e) // Cancel button event handler
+        {
+            Close();
+        }
+
+        private void BrowseToAscom(object sender, EventArgs e) // Click on ASCOM logo event handler
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("http://ascom-standards.org/");
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ascom_Driver/AAF/FocusAAF2/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FocusTest
+{
+    public class Utility
+    {
+        public static string currentTime()
+        {
+            return DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
