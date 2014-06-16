86939c0348521d8451592711684f4139f13d64be
tekkydave <tekkydave@96f2b3c2-ac1f-4b8f-8044-93e5e60fcbfa>
2014-06-16 13:00:23 +0000

    Initial Import

---
+++ AAF2Test/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AAF2Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string id = ASCOM.DriverAccess.Focuser.Choose("");
+            if (string.IsNullOrEmpty(id))
+                return;
+            // create this device
+            ASCOM.DriverAccess.Focuser focuser = new ASCOM.DriverAccess.Focuser(id);
+
+            // now run some tests, adding code to your driver so that the tests will pass.
+            // these first tests are common to all drivers.
+            Console.WriteLine("name " + focuser.Name);
+            Console.WriteLine("description " + focuser.Description);
+            Console.WriteLine("DriverInfo " + focuser.DriverInfo);
+            Console.WriteLine("driverVersion " + focuser.DriverVersion);
+
+            // Connect to Focusser
+            focuser.Connected = true;
+
+            if (focuser.Connected)
+            {
+                Console.WriteLine("Connected");
+            }
+            else
+            {
+                Console.WriteLine("Not Connected");
+            }
+
+
+            // Tests
+            commandTest(focuser, "V#");
+            
+            // Disconnect from Focusser
+            focuser.Connected = false;
+            Console.WriteLine("Press Enter to finish");
+            Console.ReadLine();
+        }
+
+        private static void commandTest(ASCOM.DriverAccess.Focuser focuser, String s)
+        {
+            Console.WriteLine();
+            Console.WriteLine("--------- S T A R T -------------");
+            Console.WriteLine(" Sending: " + s);
+            String r = focuser.CommandString(s, true);
+            Console.WriteLine("Received: " + r);
+            Console.WriteLine("------- F I N I S H -------------");
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ FocusAAF2/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FocusTest
+{
+    public partial class frmMain : Form
+    {
+        private Device device;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            btnDown.Enabled = false;
+            btnUp.Enabled = false;
+            btnDownN1.Enabled = false;
+            btnUpN1.Enabled = false;
+            btnDownN2.Enabled = false;
+            btnUpN2.Enabled = false;
+            btnConnect.Enabled = false;
+            txtStepsizeN1.Enabled = false;
+            txtStepsizeN2.Enabled = false;
+            txtGoto.Enabled = false;
+            btnGoto.Enabled = false;
+            txtPosition.Enabled = false;
+
+            // setup focuser device
+            device = new Device(rtbLog);
+            device.StepsizeN1 = FocusAAF2.Properties.Settings.Default.stepsizeN1Default;
+            device.StepsizeN2 = FocusAAF2.Properties.Settings.Default.stepsizeN2Default;
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            if (device.choose())
+            {
+                txtFocuser.Text = device.getFocuserDescription();
+                btnConnect.Enabled = true;
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (!device.isConnected())
+            {
+                if (device.connect())
+                {
+                    btnConnect.Text = "Disconnect";
+                    txtPosition.Text = device.getPosition();
+                    btnDown.Enabled = true;
+                    btnUp.Enabled = true;
+                    btnDownN1.Enabled = true;
+                    btnUpN1.Enabled = true;
+                    btnDownN2.Enabled = true;
+                    btnUpN2.Enabled = true;
+                    txtStepsizeN1.Enabled = true;
+                    txtStepsizeN2.Enabled = true;
+                    txtGoto.Enabled = true;
+                    btnGoto.Enabled = true;
+                    txtPosition.Enabled = true;
+                    txtStepsizeN1.Text = Convert.ToString(device.StepsizeN1);
+                    txtStepsizeN2.Text = Convert.ToString(device.StepsizeN2);
+
+                    rtbLog.AppendText(Utility.currentTime() + " - Connected to Device\n");
+                }
+            }
+            else
+            {
+                device.disconnect();
+
+                btnConnect.Text = "Connect";
+                btnDown.Enabled = false;
+                btnUp.Enabled = false;
+                btnDownN1.Enabled = false;
+                btnUpN1.Enabled = false;
+                btnDownN2.Enabled = false;
+                btnUpN2.Enabled = false;
+                txtStepsizeN1.Enabled = false;
+                txtStepsizeN2.Enabled = false;
+                txtGoto.Enabled = false;
+                btnGoto.Enabled = false;
+                txtPosition.Enabled = false;
+
+                rtbLog.AppendText(Utility.currentTime() + " - Disconnected from Device\n");
+            }
+            
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            if (!device.isConnected())
+            {
+                return;
+            }
+            else
+            {
+                device.moveDown();
+                string pos = device.getPosition();
+                txtPosition.Text = pos;
+                rtbLog.AppendText(Utility.currentTime() + " - Down 1 to " + pos + "\n");
+            }
+        }
+
+        private void btnDownN1_Click(object sender, EventArgs e)
+        {
+            if (!device.isConnected())
+            {
+                return;
+            }
+            else
+            {
+                string currentPos = device.getPosition();
+                device.moveTo(Int32.Parse(currentPos) - device.StepsizeN1);
+                string newPos = device.getPosition();
+                txtPosition.Text = newPos;
+                rtbLog.AppendText(Utility.currentTime() + " - Down " + Convert.ToString(device.StepsizeN1) + " to " + newPos + "\n");
+            }
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            if (!device.isConnected())
+            {
+                return;
+            }
+            else
+            {
+                device.moveUp();
+                string pos = device.getPosition();
+                txtPosition.Text = device.getPosition();
+                rtbLog.AppendText(Utility.currentTime() + " - Up 1 to " + pos + "\n");
+            }
+        }
+
+        private void btnClearLog_Click(object sender, EventArgs e)
+        {
+            rtbLog.Clear();
+        }
+
+        private void txtPosition_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpN1_Click(object sender, EventArgs e)
+        {
+            if (!device.isConnected())
+            {
+                return;
+            }
+            else
+            {
+                string currentPos = device.getPosition();
+                device.moveTo(Int32.Parse(currentPos) + device.StepsizeN1);
+                string newPos = device.getPosition();
+                txtPosition.Text = newPos;
+                rtbLog.AppendText(Utility.currentTime() + " - Up " + Convert.ToString(device.StepsizeN1) + " to " + newPos + "\n");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Are you sure you want to Exit?",
+                        "Confirm Exit",
+                        MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtStepsizeN1_TextChanged(object sender, EventArgs e)
+        {
+            device.StepsizeN1 = Convert.ToInt32(txtStepsizeN1.Text);
+        }
+
+        private void txtStepsizeN2_TextChanged(object sender, EventArgs e)
+        {
+            device.StepsizeN2 = Convert.ToInt32(txtStepsizeN2.Text);
+        }
+
+        private void btnGoto_Click(object sender, EventArgs e)
+        {
+            device.moveTo(Convert.ToInt32(txtGoto.Text));
+            string newPos = device.getPosition();
+            txtPosition.Text = newPos;
+            rtbLog.AppendText(Utility.currentTime() + " - Goto " + txtGoto.Text + ". Now at " + newPos + "\n");
+        }
+
+        private void btnDownN2_Click(object sender, EventArgs e)
+        {
+            if (!device.isConnected())
+            {
+                return;
+            }
+            else
+            {
+                string currentPos = device.getPosition();
+                device.moveTo(Int32.Parse(currentPos) - device.StepsizeN2);
+                string newPos = device.getPosition();
+                txtPosition.Text = newPos;
+                rtbLog.AppendText(Utility.currentTime() + " - Down " + Convert.ToString(device.StepsizeN2) + " to " + newPos + "\n");
+            }
+        }
+
+        private void btnUpN2_Click(object sender, EventArgs e)
+        {
+            if (!device.isConnected())
+            {
+                return;
+            }
+            else
+            {
+                string currentPos = device.getPosition();
+                device.moveTo(Int32.Parse(currentPos) + device.StepsizeN2);
+                string newPos = device.getPosition();
+                txtPosition.Text = newPos;
+                rtbLog.AppendText(Utility.currentTime() + " - Up " + Convert.ToString(device.StepsizeN2) + " to " + newPos + "\n");
+            }
+        }
+
+
+    }
+}
